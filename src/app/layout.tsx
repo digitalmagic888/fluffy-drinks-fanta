8f661a8fdd68b8dbcc37cf36f3e22792a8c03073
digitalmagic888 <32527778+digitalmagic888@users.noreply.github.com>
2023-08-10 19:26:03 -0700

    kick push
    fanta fanta, do you want to fanta

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Fluffy Drinks Fanta',
+  description: 'HOA Rewards token empowered by the memetic influence of Gabriel Iglesias aka Fluffy whom drinks Fanta',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Head from 'next/head';
+import styles from './styles/Home.module.css';
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+      <link rel="icon" href="./favicon.ico" />
+        <title>Fluffy Drinks Fanta</title>
+      </Head>
+      <header className={styles.header}>
+        <h1>🍊Fluffy Drinks Fanta🍊</h1>
+        <div className={styles.imageContainer}>
+          <img src="./fluffy-coin-logo.jpg" alt="Fluffy Coin Logo" />
+          <div className={styles.imageText}>
+            <p>Total Supply: 4.20B</p>
+            <p>6% tax tokenomics</p>
+            <p>3% HOA Rewards</p>
+            <p>2% LP</p>
+            <p>1% Marketing Wallet</p>
+            <a href="https://app.pulsex.com/swap?outputCurrency=0x01287409d65Afb751261e5A7AB8B286770753734&chain=pulsechain" className={styles.btn}>Buy $FLUFFY</a>
+          </div>
+        </div>
+      </header>
+      <main>
+        <div id="dexscreener-embed">
+          <iframe src="https://dexscreener.com/pulsechain/0x4e3b22eD9D630353888A830EA2bC25c9419Bd03A?embed=1&info=0"></iframe>
+        </div>
+      </main>
+      <footer>
+        <p>Contract Address: 0x01287409d65Afb751261e5A7AB8B286770753734</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Home;
